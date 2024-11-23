2225f7ed7c00161c47d87a693b79681ec32c3bb9
Ilyes512 <ilyes.ahidar@gmail.com>
2024-11-23 16:12:46 +0100

    Added spatie/phpunit-snapshot-assertions composer package

--- tests/Feature/FeatureTestCase.php
+++ tests/Feature/FeatureTestCase.php
@@ -11,10 +11,12 @@ use Illuminate\Support\DateFactory;
 use Ilyes512\HttpLogger\HttpLoggerServiceProvider;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Orchestra\Testbench\TestCase as OrchestraTestbench;
+use Spatie\Snapshots\MatchesSnapshots;
 
 abstract class FeatureTestCase extends OrchestraTestbench
 {
     use MockeryPHPUnitIntegration;
+    use MatchesSnapshots;
 
     /**
      * Setup the test environment.
@@ -72,5 +74,17 @@ abstract class FeatureTestCase extends OrchestraTestbench
         $app['config']['app.timezone'] = $this->getApplicationTimezone($app);
 
         DateFactory::use(CarbonImmutable::class);
+    }
+
+    protected function assertSnapshotShouldBeCreated(string $snapshotFileName): void
+    {
+        if ($this->shouldCreateSnapshots()) {
+            return;
+        }
+        static::fail(
+            "Snapshot \"$snapshotFileName\" does not exist.\n" .
+            "You can automatically create it by running \"composer update-test-snapshots\".\n" .
+            'Make sure to inspect the created snapshot afterwards to ensure its correctness!',
+        );
     }
 }
--- tests/Unit/UnitTestCase.php
+++ tests/Unit/UnitTestCase.php
@@ -6,8 +6,22 @@ namespace Ilyes512\HttpLogger\Tests\Unit;
 
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use Spatie\Snapshots\MatchesSnapshots;
 
 abstract class UnitTestCase extends PHPUnitTestCase
 {
     use MockeryPHPUnitIntegration;
+    use MatchesSnapshots;
+
+    protected function assertSnapshotShouldBeCreated(string $snapshotFileName): void
+    {
+        if ($this->shouldCreateSnapshots()) {
+            return;
+        }
+        static::fail(
+            "Snapshot \"$snapshotFileName\" does not exist.\n" .
+            "You can automatically create it by running \"composer update-test-snapshots\".\n" .
+            'Make sure to inspect the created snapshot afterwards to ensure its correctness!',
+        );
+    }
 }
